35acad7eb55ce5c29dec8ebaac456af6c5d14265
Sanil Sharma <sanil.sharma@BRILLIO.COM>
2019-02-21 15:14:45 +0530

    First Commit

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import ExpenseDashBoardPage from "../components/ExpenseDashBoardPage/ExpenseDashBoardPage";
-import AddExpensePage from "../components/AddExpensePage/AddExpensePage";
-import EditExpensePage from "../components/EditExpensePage/EditExpensePage";
-import HelpPage from "../components/HelpPage/HelpPage";
-import NotFoundPage from "../components/NotFoundPage/NotFoundPage";
+import ExpenseDashBoardPage from "../components/ExpenseDashBoardPage";
+import AddExpensePage from "../components/AddExpensePage";
+import EditExpensePage from "../components/EditExpensePage";
+import HelpPage from "../components/HelpPage";
+import NotFoundPage from "../components/NotFoundPage";
 import Header from "../components/Header/Header";
 
 const AppRouter = () => (
