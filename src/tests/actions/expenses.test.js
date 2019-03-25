c4362a63c90174e9908737ce6ebdfed27783a825
Sanil Sharma <sanil.sharma@BRILLIO.COM>
2019-03-25 16:15:59 +0530

    some additions

---
+++ src/tests/actions/expenses.test.js
@@ -0,0 +1,19 @@
+// import React from "react";
+import { addExpense, editExpense, removeExpense } from "../../actions/expenses";
+
+test("should setup remove expense action object", () => {
+  const action = removeExpense({ id: "123abc" });
+  expect(action).toEqual({ type: "REMOVE_EXPENSE", id: "123abc" });
+});
+
+test("should setup edit expense action object", () => {
+  const action = editExpense("123abc", {
+    note: "Coffee",
+    Description: "Coffee"
+  });
+  expect(action).toEqual({
+    type: "EDIT_EXPENSE",
+    id: "123abc",
+    update: { note: "Coffee", Description: "Coffee" }
+  });
+});
