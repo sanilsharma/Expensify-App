58c1ca2617ddd823005ed61823944ec164ea6318
Sanil Sharma <sanilsharma786@gmail.com>
2019-04-11 12:24:31 +0530

    some small changes

--- src/index.js
+++ src/index.js
@@ -3,21 +3,21 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import AppRouter from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
-import "normalize.css/normalize.css";
-import "./styles/style.scss";
-
 import { addExpense } from "./actions/expenses";
 import { setTextFilter } from "./actions/filters";
 import getVisibleExpenses from "./selectors/expenses";
+import "normalize.css/normalize.css";
+import "./styles/style.scss";
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: "Water Bill", amount: 4500 }));
-store.dispatch(addExpense({ description: "Gas Bill", createdAt: 1000 }));
+store.dispatch(addExpense({ description: "Water bill", amount: 4500 }));
+store.dispatch(addExpense({ description: "Gas bill", createdAt: 1000 }));
 store.dispatch(addExpense({ description: "Rent", amount: 109500 }));
 
 const state = store.getState();
-console.log(getVisibleExpenses(state.expenses, state.filters));
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
 
 const jsx = (
   <Provider store={store}>
